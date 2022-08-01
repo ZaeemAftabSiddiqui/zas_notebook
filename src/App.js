7bcdf91a56fcc0432f072f3552ff0f42c1b81c4e
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-01 20:49:06 +0500

    add Navbar and set react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is zasNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
