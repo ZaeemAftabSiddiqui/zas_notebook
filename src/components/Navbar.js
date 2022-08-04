c1701475370ee6ab0179d519599f84a1a0a2fb04
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-04 14:46:19 +0500

    delete example of context API

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          Navbar
+          ZASnotebook
         </Link>
         <button
           className="navbar-toggler"
--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -2,23 +2,8 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "zaeem",
-    class: "1st",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "zaeemAS",
-        class: "10th",
-      });
-    }, 1000);
-  };
   return (
-    <NoteContext.Provider value={{ state, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={[]}>{props.children}</NoteContext.Provider>
   );
 };
 
