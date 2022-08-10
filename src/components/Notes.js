7448a143ca5f471fd4da0381ce531b061a2a9a5c
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-10 12:37:02 +0500

    Add notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,21 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
+import Addnote from "./Addnote";
 import NoteItem from "./NoteItem";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes } = context;
+  const { notes, addNote } = context;
   return (
-    <div className="row my-3">
-      <h2>your notes</h2>
-      {notes.map((note) => {
-        return <NoteItem note={note} />;
-      })}
-    </div>
+    <>
+      <Addnote />
+      <div className="row my-3">
+        <h2>your notes</h2>
+        {notes.map((note) => {
+          return <NoteItem key={note._id} note={note} />;
+        })}
+      </div>
+    </>
   );
 };
 
