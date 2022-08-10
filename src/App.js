1f04673d273525e5efc0bbd4829e163bc91eb6e6
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-10 12:42:22 +0500

    make Alert component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import NoteState from "./context/notes/noteState";
+import Alert from "./components/Alert";
 function App() {
   return (
     <>
       <NoteState>
         <Router>
           <Navbar />
+          <Alert message="this is amazing react project" />
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home />} />
