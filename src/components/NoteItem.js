d9d58e32b1cf70d70e4fc7242a3a40d055cc3503
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-10 12:39:01 +0500

    add d-flex to align center delete and edit icon with title

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,7 +6,11 @@ const NoteItem = (props) => {
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title"> {note.title}</h5>
+          <div className="d-flex align-items-center">
+            <h5 className="card-title"> {note.title}</h5>
+            <i className="fa-solid fa-trash mx-2"></i>
+            <i className="fa-solid fa-pen-to-square mx-2"></i>
+          </div>
           <p className="card-text">{note.description}</p>
         </div>
       </div>
