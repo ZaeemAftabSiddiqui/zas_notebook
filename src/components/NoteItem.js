42f18a3f3de1d91ecf595bcc58d91276748eb133
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-12 16:53:04 +0500

    use context to delete and update notes

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,15 +1,27 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import noteContext from "../context/notes/noteContext";
 const NoteItem = (props) => {
-  const { note } = props;
+  const context = useContext(noteContext);
+  const { deleteNote } = context;
+  const { note, updateNote } = props;
   return (
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
           <div className="d-flex align-items-center">
             <h5 className="card-title"> {note.title}</h5>
-            <i className="fa-solid fa-trash mx-2"></i>
-            <i className="fa-solid fa-pen-to-square mx-2"></i>
+            <i
+              className="fa-solid fa-trash mx-2"
+              onClick={() => {
+                deleteNote(note._id);
+              }}
+            ></i>
+            <i
+              className="fa-solid fa-pen-to-square mx-2"
+              onClick={() => {
+                updateNote(note);
+              }}
+            ></i>
           </div>
           <p className="card-text">{note.description}</p>
         </div>
