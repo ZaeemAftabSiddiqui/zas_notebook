1bf4598cc95112a0f6bb55e5bfe0d3ac74f18826
Zaeem Aftab Siddiqui <ZaeemAftabSiddiqui@gmail.com>
2022-08-13 20:45:54 +0500

    add minimum length for notes to insure notes is less then 5 characters

--- src/components/Addnote.js
+++ src/components/Addnote.js
@@ -7,11 +7,16 @@ function Addnote() {
   const [note, setnote] = useState({
     title: "",
     description: "",
-    tag: "default",
+    tag: "",
   });
   const handleClick = (e) => {
     e.preventDefault();
     addNote(note.title, note.description, note.tag);
+    setnote({
+      title: "",
+      description: "",
+      tag: "",
+    });
   };
   const onChange = (e) => {
     setnote({ ...note, [e.target.name]: e.target.value });
@@ -29,8 +34,11 @@ function Addnote() {
             className="form-control"
             id="title"
             name="title"
+            value={note.title}
             aria-describedby="emailHelp"
             onChange={onChange}
+            minLength={5}
+            required
           />
         </div>
         <div className="mb-3">
@@ -42,7 +50,10 @@ function Addnote() {
             className="form-control"
             id="description"
             name="description"
+            value={note.description}
             onChange={onChange}
+            minLength={5}
+            required
           />
         </div>
         <div className="mb-3">
@@ -54,11 +65,19 @@ function Addnote() {
             className="form-control"
             id="tag"
             name="tag"
+            value={note.tag}
             onChange={onChange}
+            minLength={5}
+            required
           />
         </div>
 
-        <button type="submit" className="btn btn-primary" onClick={handleClick}>
+        <button
+          disabled={note.title.length < 5 || note.description.length < 5}
+          type="submit"
+          className="btn btn-primary"
+          onClick={handleClick}
+        >
           Add Note
         </button>
       </form>
